39678c9701cb648790240d34f4543c4c970c9753
⚫️ <jp@bultmann.eu>
2025-01-14 14:40:21 +0100

    expose ByteOwner in toplevel

--- src/lib.rs
+++ src/lib.rs
@@ -21,6 +21,7 @@ pub mod pybytes;
 mod tests;
 
 pub use crate::bytes::ByteSource;
+pub use crate::bytes::ByteOwner;
 pub use crate::bytes::Bytes;
 pub use crate::bytes::WeakBytes;
 #[cfg(feature = "zerocopy")]
